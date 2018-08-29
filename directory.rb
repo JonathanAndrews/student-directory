0e2f8d1c20ed74e7ed6a51a515c7bf9fd8c3563f
Jonathan Andrews <jonnyandrews.jma@gmail,com>
2018-08-29 18:16:51 +0100

    Can now save student list to a file

--- directory.rb
+++ directory.rb
@@ -62,6 +62,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -79,6 +80,8 @@ def process(selection)
     when "2"
       # show the students
       show_students
+    when "3"
+      save_students
     when "9"
       # exit the program
       exit
@@ -121,6 +124,17 @@ def print_footer
   end
 end
 
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:height], student[:country_of_birth], student[:cohort].capitalize, student[:hobbies]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
 
 
 
